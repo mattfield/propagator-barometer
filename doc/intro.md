1b529b663be67046525be5d6e214d27f3bf7ac8c
Matt Field <m.field42@gmail.com>
2014-01-19 17:55:28 +0000

    initial commit
    	new file:   .gitignore
      new file:   LICENSE
    	new file:   README.md
    	new file:   doc/intro.md
    	new file:   project.clj
    	new file:   src/propagator_barometer/core.clj
    	new file:   test/propagator_barometer/core_test.clj

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to propagator-barometer
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/propagator_barometer/core_test.clj
@@ -0,0 +1,7 @@
+(ns propagator-barometer.core-test
+  (:require [clojure.test :refer :all]
+            [propagator-barometer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
